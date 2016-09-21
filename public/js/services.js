8f367e42b2ec28fc8c4623080083c063fcc212f3
Niloofar Mazhari <nl.mazhari@gmail.com>
2016-09-21 02:57:05 -0700

    Update services.js

--- public/js/services.js
+++ public/js/services.js
@@ -1,6 +1,6 @@
 angular.module('airlineServices', ['ngResource'])
 	.factory('Airport', function  ($resource) {
-		return $resource('/airports/:airportCode');
+		return $resource('/airports/:airportCode', {airportCode : '@airportCode'}, 'update': { method: 'PUT'});
 	})
 	.factory('Flights', function  ($resource) {
 		return $resource('/flights');
